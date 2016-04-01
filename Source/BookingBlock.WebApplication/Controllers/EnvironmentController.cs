384c2b0773f68ce6ca33bba45d0a77fc2f51a4a2
D4022631 <d4022631@live.tees.ac.uk>
2016-04-01 21:00:01 +0100

    added environment variables page so that web masters can view information about the current hosting environment.

---
+++ Source/BookingBlock.WebApplication/Controllers/EnvironmentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingBlock.WebApplication.Controllers
+{
+    public class EnvironmentController : Controller
+    {
+        // GET: Environment
+        public ActionResult EnvironmentVariables()
+        {
+            var environmentVariables = Environment.GetEnvironmentVariables();
+
+            return View(environmentVariables);
+        }
+    }
+}
