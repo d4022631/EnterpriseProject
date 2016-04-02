777253cd43ea0fc310194a926d135ff7b395bde4
D4022631 <d4022631@live.tees.ac.uk>
2016-04-02 11:00:05 +0100

    added the identity controller to test the claims in the single page  application.

---
+++ Source/BookingBlock.WebApplication/ApiControllers/IdentityController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace BookingBlock.WebApplication.ApiControllers
+{
+    public class IdentityController : BaseApiController
+    {
+        [Route("api/identity/claims")]
+        public IHttpActionResult Get()
+        {
+            var r = Request;
+
+            var user = User as ClaimsPrincipal;
+
+            if (user != null)
+            {
+
+                var claims = from c in user.Claims
+                    select new
+                    {
+                        type = c.Type,
+                        value = c.Value
+                    };
+
+                return Json(claims);
+            }
+
+            // if we are here then the user object is null, this idicates the caller is not authenticated
+            return StatusCode(HttpStatusCode.Unauthorized);
+        }
+    }
+}
