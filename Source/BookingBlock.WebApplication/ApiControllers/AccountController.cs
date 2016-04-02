f4439f5c0416763d2949b1bb1e171040e2b9134b
D4022631 <d4022631@live.tees.ac.uk>
2016-04-02 23:45:29 +0100

    added countries list to the api account.

---
+++ Source/BookingBlock.WebApplication/ApiControllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace BookingBlock.WebApplication.ApiControllers
+{
+ 
+
+    public class AccountController : BaseApiController
+    {
+        [HttpGet, Route("api/account/countries")]
+        public async Task<IHttpActionResult> Countries()
+        {
+            CultureInfo[] specificCultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+
+            List< string > countries = new List<string>();
+
+            foreach (CultureInfo specificCulture in specificCultures)
+            {
+                var lcid = specificCulture.LCID;
+
+                RegionInfo regionInfo = new RegionInfo(lcid);
+
+                if (!countries.Contains(regionInfo.EnglishName))
+                {
+                    countries.Add(regionInfo.EnglishName);
+                }
+
+               
+            }
+
+            await Task.Run(() => countries.Sort());
+
+            return Ok(countries);
+        } 
+    }
+}
