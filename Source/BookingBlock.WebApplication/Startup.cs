3fae8777ffdb946151791e3254f0e6d96452bdef
D4022631 <d4022631@live.tees.ac.uk>
2016-04-02 22:39:53 +0100

    removed api route.

--- Source/BookingBlock.WebApplication/Startup.cs
+++ Source/BookingBlock.WebApplication/Startup.cs
@@ -56,11 +56,11 @@ namespace BookingBlock.WebApplication
 
             SwaggerConfig.Register(config);
 
-            config.Routes.MapHttpRoute(
-    name: "DefaultApiWithAction",
-    routeTemplate: "api/{controller}/{action}",
-    defaults: new { id = RouteParameter.Optional }
-);
+//            config.Routes.MapHttpRoute(
+//    name: "DefaultApiWithAction",
+//    routeTemplate: "api/{controller}/{action}",
+//    defaults: new { id = RouteParameter.Optional }
+//);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
