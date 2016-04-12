e42ff7ae62b01fa62a4e77676046562aa428b0d6
d4022631@live.tees.ac.uk <d4022631@live.tees.ac.uk>
2016-04-12 18:33:21 +0100

    added media table for business'.

--- Source/BookingBlock.WebApplication/Models/Business.cs
+++ Source/BookingBlock.WebApplication/Models/Business.cs
@@ -7,6 +7,27 @@ using System.Data.Entity.Spatial;
 namespace BookingBlock.WebApplication.Models
 {
 
+    /// <summary>
+    /// Represents media that can be added to a business page, videos, pdfs, images, etc...
+    /// </summary>
+    public class Media
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Business Business {get; set;}
+
+        [Required, ForeignKey(nameof(Business))]
+        public Guid BusinessId {get; set;}
+
+        public string Url {get; set;}
+    
+        public string Description {get; set;}
+
+        public string Title {get; set;}
+    }
 
     public class Business
     {
