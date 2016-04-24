1b9c54170966545d34a042557c19dd6a5db9bc10
D4022631 <d4022631@live.tees.ac.uk>
2016-04-24 14:01:58 +0100

    removed search distance.

--- Source/BookingBlock.WebApplication/ApiControllers/SearchController.cs
+++ Source/BookingBlock.WebApplication/ApiControllers/SearchController.cs
@@ -69,7 +69,8 @@ namespace BookingBlock.WebApplication.ApiControllers
             double distanceInMeters = GeoUtils.MilesToMeters(distanceInMiles);
 
 
-            var q = applicationDbContext.Businesses.Where(  t => t.BusinessTypeId == businessType2.Id && t.Location.Distance(searchLocation) < distanceInMeters).OrderBy(f => f.Location.Distance(searchLocation));
+            //   var q = applicationDbContext.Businesses.Where(  t => t.BusinessTypeId == businessType2.Id && t.Location.Distance(searchLocation) < distanceInMeters).OrderBy(f => f.Location.Distance(searchLocation));
+            var q = applicationDbContext.Businesses.Where(t => t.BusinessTypeId == businessType2.Id);
 
 
             SearchResponse searchResponse = new SearchResponse();
