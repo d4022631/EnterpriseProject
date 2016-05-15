67fe9d84ac3ebcde53b9a849034ec85c6e58d6c8
Peter James Umpleby <Peter James Umpleby@DESKTOP-DQSNOFC>
2016-05-15 12:04:28 +0100

    added more info to the search controller.

--- Source/BookingBlock.WebApplication/ApiControllers/SearchController.cs
+++ Source/BookingBlock.WebApplication/ApiControllers/SearchController.cs
@@ -17,101 +17,108 @@ namespace BookingBlock.WebApplication.ApiControllers
         [Route("api/Search/{businessType}/{postcode}")]
         public IHttpActionResult Search(string businessType = null, string postcode = null, double distance = 10)
         {
-
-            
-
-            string userId = null;
-
             try
             {
-                var user = User as ClaimsPrincipal;
 
-                if (user != null)
+
+
+                string userId = null;
+
+                try
                 {
-                    userId = user.Identity.GetUserId();
+                    var user = User as ClaimsPrincipal;
+
+                    if (user != null)
+                    {
+                        userId = user.Identity.GetUserId();
+                    }
+
                 }
-                
-            }
-            catch (Exception)
-            {
-                
-                //throw;
-            }
-
-            var
-           applicationDbContext = db;
-
-            var businessType2 = db.BusinessTypes.FirstOrDefault(type => type.Name == businessType);
-
-            if (businessType2 == null)
-            {
-                return Content(HttpStatusCode.NotFound, "The given business type could not be found in the database");
-            }
-
-            var searchLocation = PostcodesService.Lookup(postcode);
-
-
-
-            //  SearchStore searchStore = new SearchStore(applicationDbContext);
-
-            // searchStore.Search(postcode, businessType);
-
-
-
-            double distanceInMiles = distance;
-
-            double distanceInMeters = GeoUtils.MilesToMeters(distanceInMiles);
-
-
-            var searchResults =
-                applicationDbContext.Businesses.Where(
-                    t => t.BusinessTypeId == businessType2.Id && t.Location.Distance(searchLocation) <= distanceInMeters)
-                    .OrderBy(business => business.Location.Distance(searchLocation));
-
-            //var q = applicationDbContext.Businesses;
-
-
-            SearchResponse searchResponse = new SearchResponse();
-
-            searchResponse.BusinessType = businessType2.Name;
-            searchResponse.BusinessTypeId = businessType2.Id;
-            searchResponse.Postcode = postcode;
-
-            
-
-            searchResponse.Latitude = searchLocation.Latitude.Value;
-            searchResponse.Longitude = searchLocation.Longitude.Value;
-
-            searchResponse.Within = distance;
-            searchResponse.WithinM = distanceInMeters;
-
-            List<BusinessSearchResult> results = new List<BusinessSearchResult>();
-
-            foreach (Business business in searchResults)
-            {
-
-                double distanceFromPostcode = business.Location.Distance(searchLocation).Value;
-
-                var result = new BusinessSearchResult()
+                catch (Exception)
                 {
-                    Distance = Math.Round(distanceFromPostcode, 0),
-                    Name = business.Name,
-                    BusinessId = business.Id,
-                    Latitude = business.Location.Latitude,
-                    Longitude = business.Location.Longitude,
-                    Postcode = business.Postcode,
-                    Address = business.Address,
-                    Website = business.Website,
-                    
-                };
 
-               results.Add(result);
+                    //throw;
+                }
 
+                var
+               applicationDbContext = db;
+
+                var businessType2 = db.BusinessTypes.FirstOrDefault(type => type.Name == businessType);
+
+                if (businessType2 == null)
+                {
+                    return Content(HttpStatusCode.NotFound, "The given business type could not be found in the database");
+                }
+
+                var searchLocation = PostcodesService.Lookup(postcode);
+
+
+
+                //  SearchStore searchStore = new SearchStore(applicationDbContext);
+
+                // searchStore.Search(postcode, businessType);
+
+
+
+                double distanceInMiles = distance;
+
+                double distanceInMeters = GeoUtils.MilesToMeters(distanceInMiles);
+
+
+                var searchResults =
+                    applicationDbContext.Businesses.Where(
+                        t => t.BusinessTypeId == businessType2.Id && t.Location.Distance(searchLocation) <= distanceInMeters)
+                        .OrderBy(business => business.Location.Distance(searchLocation));
+
+                //var q = applicationDbContext.Businesses;
+
+
+                SearchResponse searchResponse = new SearchResponse();
+
+                searchResponse.BusinessType = businessType2.Name;
+                searchResponse.BusinessTypeId = businessType2.Id;
+                searchResponse.Postcode = postcode;
+
+
+
+                searchResponse.Latitude = searchLocation.Latitude.Value;
+                searchResponse.Longitude = searchLocation.Longitude.Value;
+
+                searchResponse.Within = distance;
+                searchResponse.WithinM = distanceInMeters;
+
+                List<BusinessSearchResult> results = new List<BusinessSearchResult>();
+
+                foreach (Business business in searchResults)
+                {
+
+                    double distanceFromPostcode = business.Location.Distance(searchLocation).Value;
+
+                    var result = new BusinessSearchResult()
+                    {
+                        Distance = Math.Round(distanceFromPostcode, 0),
+                        Name = business.Name,
+                        BusinessId = business.Id,
+                        Latitude = business.Location.Latitude,
+                        Longitude = business.Location.Longitude,
+                        Postcode = business.Postcode,
+                        Address = business.Address,
+                        Website = business.Website,
+
+                    };
+
+                    results.Add(result);
+
+                }
+
+                searchResponse.Results = results;
+
+                return Ok(searchResponse);
             }
-
-            searchResponse.Results = results;
-
-            return Ok(searchResponse);
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
     }
 }
